8f60574715e784f49507e2b41606178289eb3d8c
longgpgp2 <longnguyenthanh2003@gmail.com>
2025-04-17 11:08:09 +0700

    Home page and About page

--- src/modules/pages/about/about.component.ts
+++ src/modules/pages/about/about.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faMapLocation, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  faLocation: IconDefinition = faMapLocation;
+  faPhone: IconDefinition = faPhone;
+  faEnvelope: IconDefinition = faEnvelope;
 }
